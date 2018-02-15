25a03575aa055e9e36a6705bbc573d688a277029
I. Salikov <salikovpro@gmail.com>
2018-02-15 19:50:12 +0500

    Fix CI delay

--- __tests__/auth.spec.js
+++ __tests__/auth.spec.js
@@ -1,7 +1,7 @@
 const request = require('supertest')
 const app = require('../app')
 const mongoose = require('../libs/mongoose')
-const CIDELAY = 30000
+const CIDELAY = 300000
 
 describe('/auth/:email', () => {
     afterAll(() => {
